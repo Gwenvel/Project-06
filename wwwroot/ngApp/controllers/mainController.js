79de0557ee20023aad2ccd262e0cf5d835ec6140
Joy Kimaru <31221028+Gwenvel@users.noreply.github.com>
2017-08-28 15:22:26 -0700

    fixed the random generator

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -5,7 +5,7 @@ class MainController {
         randomFortune(){
         let words = "";
         let fortunes = ["Have a great day", "Shine bright", "Good luck", "Hello from the other side!!!", "Your on the wrong page...Please refreash","My favorite number is 11","I am a DALEK!!! EXTERMINATE"];
-        words= fortunes[Math.floor(Math.random() * fortunes.length-1)];
+        words= fortunes[Math.floor(Math.random() * fortunes.length)];
        
         return words;
     }

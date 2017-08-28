c29ab8ce6c6d09180700755bbe5078ad861a134c
aja025 <andrew.antila2@gmail.com>
2017-08-28 14:52:21 -0700

    part 1 edits

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -1,5 +1,12 @@
 class MainController {
     constructor() {
-        this.message = "hello from main";
+        this.message = this.randomFortune();
+    }
+        randomFortune(){
+        let words = "";
+        let fortunes = ["Have a great day", "Shine bright", "Good luck"];
+        words= fortunes[Math.floor(Math.random() * 3)];
+       
+        return words;
     }
 }

3a6949383db6ccf0c9ba32fde174d718059e5ad7
Joy Kimaru <Joy Kimaru>
2017-08-28 14:50:33 -0700

    changes

--- wwwroot/ngApp/controllers/mainController.js
+++ wwwroot/ngApp/controllers/mainController.js
@@ -1,5 +1,13 @@
 class MainController {
     constructor() {
-        this.message = "hello from main";
+
+
+
+        let phrases = ["Hello from the other side!!!", "Your on the wrong page...Please refreash","My favorite number is 11","I am a DALEK!!! EXTERMINATE"];
+
+        this.message = phrases[Math.floor((Math.random() * phrases.length))];
+   
+           
     }
+
 }
